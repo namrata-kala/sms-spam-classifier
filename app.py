14eedd237a6d225e51cc956e41034eaa89b9d9f0
Namrata kala <namratakala@Namratas-MacBook-Air-591.local>
2026-02-15 00:31:52 +0530

    Fixed nltk cloud path issue

--- app.py
+++ app.py
@@ -4,8 +4,16 @@ import string
 import nltk
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
-nltk.download('punkt')
-nltk.download('stopwords')
+import os
+
+nltk_data_path = os.path.join(os.getcwd(), "nltk_data")
+os.makedirs(nltk_data_path, exist_ok=True)
+nltk.data.path.append(nltk_data_path)
+
+nltk.download("punkt", download_dir=nltk_data_path)
+nltk.download("punkt_tab", download_dir=nltk_data_path)
+nltk.download("stopwords", download_dir=nltk_data_path)
+
 
 ps = PorterStemmer()
 
